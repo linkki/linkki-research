83f393b1408130e3496907b26d04d646d510d6a7
Iikka Hauhio <iikka.hauhio@helsinki.fi>
2019-11-16 22:15:35 +0200

    koodien avaaminen ja tallentaminen

--- pohja/pohja.js
+++ pohja/pohja.js
@@ -1,6 +1,22 @@
+function elem(name, attrs, children) {
+	const element = document.createElement(name);
+	for (const key of Object.keys(attrs || {})) {
+		element[key] = attrs[key];
+	}
+	for (const child of children || []) {
+		element.appendChild(child);
+	}
+	return element;
+}
 
+const editors = {};
+let dialogAdded = false;
 let editorCounter = 0;
 function addEditor(value, readOnly, lang) {
+	if (!dialogAdded) {
+		addDialog();
+		dialogAdded = true;
+	}
 	const number = editorCounter++;
 	if (readOnly) {
 		document.write(`
@@ -14,7 +30,7 @@ function addEditor(value, readOnly, lang) {
 		document.write(`
 		<div class="split-screen">
 			<div class="left editor-side">
-				<div class="toolbar"><button id="update${number}">🏴</button></div>
+				<div class="toolbar"><button id="update${number}">🏴</button><button id="open${number}">Avaa</button><button id="save${number}">Tallenna</button></div>
 				<div class="editor" id="editor${number}"></div>
 			</div>
 			<div class="right result-side">
@@ -23,6 +39,7 @@ function addEditor(value, readOnly, lang) {
 		</div>`);
 	}
 	const editor = ace.edit(`editor${number}`);
+	editors[number] = editor;
 	editor.setTheme("ace/theme/eclipse");
 	editor.session.setMode("ace/mode/" + (lang || "html"));
 	editor.setOptions({
@@ -53,6 +70,17 @@ function addEditor(value, readOnly, lang) {
 	} else {
 		document.querySelector(`#update${number}`).onclick = update;
 		//editor.session.on("change", update);
+
+		document.querySelector(`#open${number}`).onclick = async () => {
+			const content = await openFile();
+			if (!content) return;
+			editor.setValue(content);
+			editor.clearSelection();
+		};
+
+		document.querySelector(`#save${number}`).onclick = async () => {
+			await saveFile(editor.getValue());
+		};
 	}
 }
 function codeExample(value, lang, small) {
@@ -72,8 +100,129 @@ function codeExample(value, lang, small) {
 		highlightActiveLine: false,
 		fontSize: small ? "small" : "large",
 	});
-	editor.renderer.$cursorLayer.element.style.display = "none"
+	editor.renderer.$cursorLayer.element.style.display = "none";
 	editor.setValue(value);
 	editor.clearSelection();
 }
-let closeScript = "</script>";
+const closeScript = "</script>";
+
+let folder = null;
+let file = null;
+let password = null;
+const passwords = {};
+
+let fileAction = null;
+let fileContent = null;
+let finishAction = null;
+
+function saveFile(content) {
+	return new Promise((resolve, reject) => {
+		fileAction = saveFileAction;
+		fileContent = content;
+		finishAction = resolve;
+		document.querySelector("#dialog").style.display = "block";
+	});
+}
+
+async function saveFileAction() {
+	const formData = new FormData();
+	formData.set("password", password);
+	formData.set("content", fileContent);
+	await fetch(`https://sorsa.herokuapp.com/folder/${folder}/document/${file}`, {
+		method: "PUT",
+		body: formData
+	});
+}
+
+function openFile() {
+	return new Promise((resolve, reject) => {
+		fileAction = openFileAction;
+		finishAction = resolve;
+		document.querySelector("#dialog").style.display = "block";
+	});
+}
+
+async function openFileAction() {
+	const response = await fetch(`https://sorsa.herokuapp.com/folder/${folder}/document/${file}?password=${encodeURIComponent(password)}`, {
+		method: "GET"
+	});
+	const text = await response.text();
+	return text;
+}
+
+function closeDialog(result) {
+	document.querySelector("#folderContent").innerHTML = "";
+	document.querySelector("#fileName").value = "";
+	document.querySelector("#fileForm").style.display = "none";
+	document.querySelector("#dialog").style.display = "none";
+	file = folder = password = null;
+	finishAction(result);
+}
+
+function addDialog() {
+	const dialog = elem("div", {
+		className: "dialog",
+		id: "dialog",
+		innerHTML: `
+<button id="cancel">Peruuta</button>
+<h2>Valitse tiedosto</h2>
+<form id="folderForm">
+  <input type="text" id="folderName" placeholder="Kansio">
+  <input type="submit" value="Avaa kansio">
+</form>
+<div id="folderContent"></div>
+<form id="fileForm" style="display: none;">
+  <input type="text" id="fileName" placeholder="Tiedosto">
+  <input type="submit" value="Valitse tiedosto">
+</form>
+`});
+	dialog.style.display = "none";
+	document.querySelector("body").appendChild(dialog);
+	document.querySelector("#folderForm").onsubmit = async event => {
+		event.preventDefault();
+		const folderName = document.querySelector("#folderName").value;
+		try {
+			const givenPassword = passwords[folderName] || prompt("Anna kansion salasana.");
+			const response = await fetch(`https://sorsa.herokuapp.com/folder/${folderName}/list?password=${encodeURIComponent(givenPassword)}`, {method: "GET"});
+			const listing = await response.json();
+			const table = elem("table", {
+				innerHTML: `<tr><th>Nimi</th><th>Koko</th></tr>`,
+			});
+			for (const file of listing) {
+				const button = elem("button", {
+					onclick: () => {
+						document.querySelector("#fileName").value = file.name;
+					},
+					textContent: file.name,
+				});
+				let nameTd, sizeTd;
+				table.appendChild(elem("tr", {}, [nameTd=elem("td"), sizeTd=elem("td")]));
+				nameTd.appendChild(button);
+				sizeTd.textContent = file.size + " tavua";
+			}
+			document.querySelector("#folderContent").innerHTML = "";
+			document.querySelector("#folderContent").appendChild(table);
+			document.querySelector("#fileForm").style.display = "block";
+			folder = folderName;
+			password = givenPassword;
+			passwords[folder] = password;
+		} catch (error) {
+			alert(error);
+		}
+	};
+	document.querySelector("#fileForm").onsubmit = async event => {
+		event.preventDefault();
+		let result = undefined;
+		try {
+			const fileName = document.querySelector("#fileName").value;
+			file = fileName;
+			result = await fileAction();
+		} catch (error) {
+			alert(error);
+		}
+		closeDialog(result);
+	};
+	document.querySelector("#cancel").onclick = async event => {
+		closeDialog(undefined);
+	};
+}
